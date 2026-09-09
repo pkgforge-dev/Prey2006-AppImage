f233a002613d388515241120e3e2d47ba8b3117b
Link4Electronics <link4electronics@gmail.com>
2026-09-09 02:09:41 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -23,7 +23,7 @@ mkdir -p ./AppDir/bin
 cd Prey2006/neo
 if [ "$ARCH" = "aarch64" ]; then
 	#sed -i 's/(int)(const char\*)command.parmList/(int)(intptr_t)(const char*)command.parmList/g' Prey/game_anim.cpp
-  apply ../../hhprofiler-aarch64.patch
+  git apply ../../hhprofiler-aarch64.patch
 fi
 cmake . -DCMAKE_BUILD_TYPE=Release -DSDL3=ON
 make -j$(nproc)
